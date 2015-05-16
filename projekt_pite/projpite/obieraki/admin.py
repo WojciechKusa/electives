5cc669fec775a6544869956915ada245139623e8
hxc0 <adam.dudzic@gmail.com>
2015-05-16 20:28:06 +0200

    dodany admin

--- projekt_pite/projpite/obieraki/admin.py
+++ projekt_pite/projpite/obieraki/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.register(User_2)
+admin.site.register(Student)
+admin.site.register(Course)
+admin.site.register(Class)
 
 # Register your models here.
